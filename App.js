1177c8da73a3e89c61a0dbef4f489d9da20376b6
Clare Henderson <henderson2350@gmail.com>
2021-09-30 14:48:18 -0400

    basics

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+var image = require('./assets/favicon.png')
 
-export default function App() {
+function App() {
+  const handlePress = () => console.log("Text clicked");
+
   return (
-    <View style={styles.container}>
-      <Text>Line Checker</Text>
-      <StatusBar style="auto" />
-    </View>
+    //View is like a div
+    //Safe area view keeps it beyond the notch
+    //Text always has to be wrapped in a text component
+    <SafeAreaView style={styles.container}>
+      <Text onPress = {handlePress}>Line Checker</Text>
+      <Image source={image} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    //view is flexible and takes up the entire screen
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default App;
