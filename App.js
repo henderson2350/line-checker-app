66289fc3e8b1f4a29f47d7f30e64f4b2363fdd01
Clare Henderson <henderson2350@gmail.com>
2021-10-07 11:53:11 -0400

    logo added, some functionality

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
-var image = require('./assets/favicon.png')
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity} from "react-native";
+var image = require("./assets/bigLogo.png");
 
 function App() {
   const handlePress = () => console.log("Text clicked");
@@ -11,8 +11,10 @@ function App() {
     //Safe area view keeps it beyond the notch
     //Text always has to be wrapped in a text component
     <SafeAreaView style={styles.container}>
-      <Text onPress = {handlePress}>Line Checker</Text>
-      <Image source={image} />
+      <TouchableOpacity onPress={handlePress}>
+      <Image onPress={handlePress} source={image} />
+      </TouchableOpacity>
+      {/* <Text onPress={handlePress}>Line Checker</Text> */}
     </SafeAreaView>
   );
 }
@@ -21,9 +23,9 @@ const styles = StyleSheet.create({
   container: {
     //view is flexible and takes up the entire screen
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
